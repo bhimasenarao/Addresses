51e0c227832b0520e02af7d31ed3e104399470a4
Bhimasena Patri <himasena.rao@gmail.com>
2017-06-10 18:33:56 +0800

    First Commit

---
+++ Ads/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Ads
+//
+//  Created by Bhimasena Patri on 10/6/2017.
+//  Copyright © 2017 Bhimasena Patri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblView: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    lblView.text = "Addresses"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
